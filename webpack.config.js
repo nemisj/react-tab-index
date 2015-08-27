00809b504f17c3f1dc31ca26693ab3cd380658c8
Maks Nemisj <maks@nemisj.com>
2015-08-27 12:02:09 +0200

    Adding hot-loader to the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: {
-    app: ["./app/main.js"]
-  },
+
+  entry: [
+    'webpack/hot/only-dev-server',
+    './app/main.js'
+  ],
+
   output: {
     path: "./build",
     publicPath: "/assets/",
     filename: "bundle.js"
-  }
+  },
+
+  module: {
+     loaders: [
+       { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+       { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'},
+       { test: /\.css$/, loader: "style!css" }
+     ]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
 };
